8a211a7ba07f7ae47c7812cd0ed9735fcd1b5ee6
Fatih KARAKAŞ <abdifaret@gmail.com>
2026-08-01 16:02:01 +0300

    formula: hiztesti 0.1.1

--- Formula/hiztesti.rb
+++ Formula/hiztesti.rb
@@ -12,27 +12,27 @@ class Hiztesti < Formula
   desc "Internet speed and quality measurement from the command line"
   homepage "https://hiztesti.com"
   license :cannot_represent # tescilli; kaynak yayımlanmıyor
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/netmetrik/hiztesti-cli/releases/download/v#{version}/hiztesti-darwin-arm64.tgz"
-      sha256 "c672398c522d88c98b2e117e1ff8ee6f76cb7b3722dc5153609b9e76aa0abacf"
+      sha256 "b0607614cbd91b52c01b054f7e8c207d9b5d88eb57e5d9a73da81ac87e99f589"
     end
     on_intel do
       url "https://github.com/netmetrik/hiztesti-cli/releases/download/v#{version}/hiztesti-darwin-x86_64.tgz"
-      sha256 "9320b75b87f50c536c9c9255c4e403fe98cca5b9e0e7c0ed2dea47cb47cd80f6"
+      sha256 "e81b3dba4ec886180d726a640feb0da8cad82cb849ddb902d89fa15dd53102c0"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/netmetrik/hiztesti-cli/releases/download/v#{version}/hiztesti-linux-aarch64.tgz"
-      sha256 "8c15f88c9fc450b04df840042149ea920f26e3905a7a30f27f9df028f459afeb"
+      sha256 "b29adc54abcc76edb5334be92161d7e8825af5d05f8f315f97415574676fda13"
     end
     on_intel do
       url "https://github.com/netmetrik/hiztesti-cli/releases/download/v#{version}/hiztesti-linux-x86_64.tgz"
-      sha256 "b782d8cc1aa41ee9968ad7a019b022ddafa2920b002bd0dd6a019cfc2ffef2ee"
+      sha256 "a23c4dc7281ac634c49da79ed562d1241c8f86064c8820202b0f981d4ec6a93d"
     end
   end
 
